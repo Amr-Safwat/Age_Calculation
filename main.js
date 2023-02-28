227e4ceafef3d33a7e9f07be679396d4e71bc7eb
Amr Safwat <amrsafwat02@gmail.com>
2023-02-28 02:26:38 +0200

    Added some Modifiers

--- main.js
+++ main.js
@@ -12,28 +12,55 @@ let result;
 let dateNow = new Date();
 
 btn.onclick = () => {
-  if (inputs[0].value !== ''&&inputs[1].value !== ''&&inputs[2].value !== '') {
-    let value = `${inputs[1].value} ${inputs[2].value} ${inputs[0].value}`;
-    let valueTwo = `${inputs[2].value}/${inputs[1].value}/${inputs[0].value}`;
-    let birthday = new Date(value);
-    result = dateNow - birthday;
-    let fullYears = parseInt(result / 1000 / 60 / 60 / 24 / 365);
-    date.innerHTML = `تاريخ ميلادك هو ${valueTwo}`;
-    age.innerHTML = `عمرك ${fullYears} سنة`;
-    day.innerHTML = `ولدت فى يوم ${showTheDay(birthday.getDay())}`;
-    days.innerHTML = `عمرك بالايام ${parseInt(result / 1000 / 60 / 60 / 24)} يوم`;
-    hours.innerHTML = `عمرك بالساعات ${parseInt(result / 1000 / 60 / 60)} ساعة`;
-    minutes.innerHTML = `عمرك بالدقائق ${parseInt(result / 1000 / 60)} دقيقة`;
+  inputs.forEach((e)=> {
+    if(e.value !== '') {
+      if (inputs[0].value !== ''&&inputs[1].value !== ''&&inputs[2].value !== '') {
+        if(!isNaN(parseInt(inputs[2].value))&&!isNaN(parseInt(inputs[2].value))&&!isNaN(parseInt(inputs[0].value))) {
+          let value = `${inputs[1].value} ${inputs[2].value} ${inputs[0].value}`;
+        let valueTwo = `${inputs[2].value}/${inputs[1].value}/${inputs[0].value}`;
+        let birthday = new Date(value);
+        result = dateNow - birthday;
+        let fullYears = parseInt(result / 1000 / 60 / 60 / 24 / 365);
+        date.innerHTML = `تاريخ ميلادك هو ${valueTwo}`;
+        age.innerHTML = `عمرك ${fullYears} سنة`;
+        day.innerHTML = `ولدت فى يوم ${showTheDay(birthday.getDay())}`;
+        days.innerHTML = `عمرك بالايام ${parseInt(result / 1000 / 60 / 60 / 24)} يوم`;
+        hours.innerHTML = `عمرك بالساعات ${parseInt(result / 1000 / 60 / 60)} ساعة`;
+        minutes.innerHTML = `عمرك بالدقائق ${parseInt(result / 1000 / 60)} دقيقة`;
 
-    modal.style.display = 'block';
+        modal.style.display = 'block';
+        }else{
 
+        }
+
+      }else{
+
+      }
+    }else {
+      e.style.border= '1px solid red';
+    }
+  });
+
+};
+inputs.forEach((e)=>{
+  if(e.value !==''){
     inputs[0].value = '';
     inputs[1].value = '';
     inputs[2].value = '';
-  } else{
-    console.log('Error');
   }
-};
+})
+// inputs.forEach((e)=> {
+//  e.value = '';
+// });
+
+for(let i = 0;i< inputs.length;i++) {
+  inputs[i].style.border = 'none';
+}
+for(let i = 0;i< inputs.length;i++) {
+  inputs[i].oninput = () => {
+    inputs[i].style.border = 'none';
+  }
+}
 
 function showTheDay(dayNum) {
   switch(dayNum) {
