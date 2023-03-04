fad13cdb33bb77c3acc635763c22987e1cf6612a
Amr Safwat <amrsafwat02@gmail.com>
2023-03-05 01:21:41 +0200

    Added some Modifiers

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let inputs = document.querySelectorAll('#date');
+let span = document.querySelector('#val');
 let date = document.querySelector('.date');
 let age = document.querySelector('.age');
 let months = document.querySelector('.months');
@@ -13,85 +14,113 @@ let result;
 let dateNow = new Date();
 
 btn.onclick = () => {
-  showAge();
+	showAgeDetails();
 };
-inputs.forEach((e)=>{
-  if(e.value !==''){
-    inputs[0].value = '';
-    inputs[1].value = '';
-    inputs[2].value = '';
-  }
-})
 
-for(let i = 0;i< inputs.length;i++) {
-  inputs[i].style.border = 'none';
+span.style.visibility = 'hidden';
+
+inputs.forEach((e) => {
+	if (e.value !== '') {
+		inputs[0].value = '';
+		inputs[1].value = '';
+		inputs[2].value = '';
+	}
+});
+
+for (let i = 0; i < inputs.length; i++) {
+	inputs[i].style.border = 'none';
 }
-for(let i = 0;i< inputs.length;i++) {
-  inputs[i].oninput = () => {
-    inputs[i].style.border = 'none';
-  }
+for (let i = 0; i < inputs.length; i++) {
+	inputs[i].oninput = () => {
+		inputs[i].style.border = 'none';
+	};
 }
 
-function showAge() {
-  inputs.forEach((e)=> {
-    if(e.value !== '') {
-      if (inputs[0].value !== ''&&inputs[1].value !== ''&&inputs[2].value !== '') {
-        if(!isNaN(parseInt(inputs[2].value))&&!isNaN(parseInt(inputs[2].value))&&!isNaN(parseInt(inputs[0].value))) {
-        if((inputs[1].value < 31&&inputs[1].value > 0)&&(inputs[2].value < 12&&inputs[2].value > 0)) {
-          let value = `${inputs[1].value} ${inputs[2].value} ${inputs[0].value}`;
-          let valueTwo = `${inputs[2].value}/${inputs[1].value}/${inputs[0].value}`;
-          let birthday = new Date(value);
-          result = dateNow - birthday;
-          let fullYears = parseInt(result / 1000 / 60 / 60 / 24 / 365);
-          date.innerHTML = `تاريخ ميلادك هو ${valueTwo}`;
-          age.innerHTML = `عمرك ${fullYears} سنة`;
-          day.innerHTML = `ولدت فى يوم ${showTheDay(birthday.getDay())}`;
-          months.innerHTML = `عمرك بالشهور ${parseInt(result / 1000 / 60 / 60 / 24 / 12)} شهر`;
-          days.innerHTML = `عمرك بالايام ${parseInt(result / 1000 / 60 / 60 / 24)} يوم`;
-          hours.innerHTML = `عمرك بالساعات ${parseInt(result / 1000 / 60 / 60)} ساعة`;
-          minutes.innerHTML = `عمرك بالدقائق ${parseInt(result / 1000 / 60)} دقيقة`;
+function showAgeDetails() {
+	inputs.forEach((e) => {
+		if (e.value !== '') {
+			if (
+				inputs[0].value !== '' &&
+				inputs[1].value !== '' &&
+				inputs[2].value !== ''
+			) {
+				if (
+					!isNaN(+inputs[0].value) &&
+					!isNaN(+inputs[1].value) &&
+					!isNaN(+inputs[2].value)
+				) {
+					if (
+						+inputs[1].value <= 12 &&
+						+inputs[1].value > 0 &&
+						+inputs[2].value <= 31 &&
+						+inputs[2].value > 0
+					) {
+						span.style.visibility = 'hidden';
+						let value = `${inputs[1].value} ${inputs[2].value} ${inputs[0].value}`;
+						let valueTwo = `${inputs[2].value}/${inputs[1].value}/${inputs[0].value}`;
+						let birthday = new Date(value);
+						result = dateNow - birthday;
+						let fullYears = parseInt(result / 1000 / 60 / 60 / 24 / 365);
+						date.innerHTML = `تاريخ ميلادك هو ${valueTwo}`;
+						age.innerHTML = `عمرك ${fullYears} سنة`;
+						day.innerHTML = `ولدت فى يوم ${showTheDay(birthday.getDay())}`;
+						months.innerHTML = `عمرك بالشهور ${parseInt(
+							result / 1000 / 60 / 60 / 24 / 12
+						)} شهر`;
+						days.innerHTML = `عمرك بالايام ${parseInt(
+							result / 1000 / 60 / 60 / 24
+						)} يوم`;
+						hours.innerHTML = `عمرك بالساعات ${parseInt(
+							result / 1000 / 60 / 60
+						)} ساعة`;
+						minutes.innerHTML = `عمرك بالدقائق ${parseInt(
+							result / 1000 / 60
+						)} دقيقة`;
 
-          modal.style.display = 'block';
-        }else {
-          console.log('error');
-        }
-        }else{
+						modal.style.display = 'block';
+					} else {
+						span.style.visibility = 'visible';
+					}
+				} else {
+				}
+			} else {
 
-        }
-
-      }else{
-
-      }
-    }else {
-      e.style.border= '1px solid red';
-    }
-  });
+			}
+		} else {
+			e.style.border = '1px solid red';
+      span.innerHTML = 'ادخل التاريخ';
+      span.style.visibility = 'visible';
+		}
+	});
 }
 
 function showTheDay(dayNum) {
-  switch(dayNum) {
-    case 0:
-      return 'الاحد';
-    case 1:
-      return 'الاثنين';
-    case 2:
-      return 'الثلاثاء';
-    case 3:
-      return 'الاربعاء';
-    case 4:
-      return 'الخميس';
-    case 5:
-      return 'الجمعة';
-    case 6:
-      return 'السبت';
-  }
+	switch (dayNum) {
+		case 0:
+			return 'الاحد';
+		case 1:
+			return 'الاثنين';
+		case 2:
+			return 'الثلاثاء';
+		case 3:
+			return 'الاربعاء';
+		case 4:
+			return 'الخميس';
+		case 5:
+			return 'الجمعة';
+		case 6:
+			return 'السبت';
+	}
 }
 
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
+
+// console.log(inputs[0]);
+console.log(inputs[1]);
